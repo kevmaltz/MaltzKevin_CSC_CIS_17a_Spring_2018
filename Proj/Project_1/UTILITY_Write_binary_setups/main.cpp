1b46c54b213272cdeb376e16ed95c8d1d1ed2cb0
Kevin Maltz <31598741+kevmaltz@users.noreply.github.com>
2018-04-08 16:13:52 -0700

    Changed format of text files. read in algorithm changed to match
    Text files show data row by row respective to the board and their columns. Each position in a row is seperated by a '#', including the endline character.

--- Proj/Project_1/UTILITY_Write_binary_setups/main.cpp
+++ Proj/Project_1/UTILITY_Write_binary_setups/main.cpp
@@ -34,29 +34,62 @@ struct Location
 
 int main(int argc, char** argv) 
 {
+    const int NUM_LOC = 65;
+    const int ROW_MX = 13;
+    const int COL_MX = 5;
     fstream txt("text_input.txt", ios::in);
     fstream bin("Setup.dat", ios::out | ios::binary);
-    Location stream;
+    Location stream[5];
     string ptrRaw;
     string tmpBool;
     
-    do
+//    do
+//    {
+//        //Get data from text file
+//        getline(txt, stream.type);
+//        getline(txt, tmpBool);
+//        tmpBool=="0"?stream.isOcp = false:stream.isOcp=true;
+//        getline(txt, tmpBool);
+//        tmpBool=="0"?stream.isRR = false:stream.isRR=true;
+//        getline(txt, ptrRaw);
+//        if(ptrRaw == "0" || ptrRaw == "NULL")
+//            stream.occUnit = NULL;
+//        else
+//            stream.occUnit = reinterpret_cast<Unit*>(strtol(ptrRaw.c_str(),NULL,0));
+//        
+//        //Write out data to binary file
+//        bin.write(reinterpret_cast<char *>(&stream), sizeof(stream));
+//    }while(!txt.eof());
+    for(int r=0; r< ROW_MX; r++)
     {
-        //Get data from text file
-        getline(txt, stream.type);
-        getline(txt, tmpBool);
-        tmpBool=="0"?stream.isOcp = false:stream.isOcp=true;
-        getline(txt, tmpBool);
-        tmpBool=="0"?stream.isRR = false:stream.isRR=true;
-        getline(txt, ptrRaw);
-        if(ptrRaw == "0" || ptrRaw == "NULL")
-            stream.occUnit = NULL;
-        else
-            stream.occUnit = reinterpret_cast<Unit*>(strtol(ptrRaw.c_str(),NULL,0));
+        for(int c=0; c < COL_MX; c++)
+            getline(txt, stream[c].type, '#');
+        txt.ignore(1000, '\n');
+        for(int c=0; c < COL_MX; c++)
+        {
+            getline(txt, tmpBool, '#');
+            tmpBool=="0"?stream[c].isOcp = false:stream[c].isOcp=true;
+        }
+        txt.ignore(1000, '\n');
+        for(int c=0; c < COL_MX; c++)
+        {
+            getline(txt, tmpBool, '#');
+            tmpBool=="0"?stream[c].isRR = false:stream[c].isRR=true;
+        }
+        txt.ignore(1000, '\n');
+        for(int c=0; c < COL_MX; c++)
+        {
+            getline(txt, ptrRaw, '#');
+            if(ptrRaw == "0" || ptrRaw == "NULL")
+                stream[c].occUnit = NULL;
+            else
+                stream[c].occUnit = reinterpret_cast<Unit*>(strtol(ptrRaw.c_str(),NULL,0));
+        }
+        txt.ignore(1000, '\n');
         
         //Write out data to binary file
         bin.write(reinterpret_cast<char *>(&stream), sizeof(stream));
-    }while(!txt.eof());
+    }
     
     txt.close();
     bin.close();
