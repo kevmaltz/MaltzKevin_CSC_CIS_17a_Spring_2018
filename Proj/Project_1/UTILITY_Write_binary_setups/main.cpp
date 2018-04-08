95cf8af70a7213557139f0af88030390060b401b
Kevin Maltz <31598741+kevmaltz@users.noreply.github.com>
2018-04-08 15:35:53 -0700

    Conversion from formatted text to binary files
    Converts formatted text files containing initial board setup into a binary file for use by the main game application

--- Proj/Project_1/UTILITY_Write_binary_setups/main.cpp
+++ Proj/Project_1/UTILITY_Write_binary_setups/main.cpp
@@ -22,8 +22,8 @@ struct Location
 {
     string type;    //What type of position is it. Camp/Battlefield/Frontline/Headquarters/Mountain
     bool isOcp;     //If location occupied, set true
-    Unit *occUnit;  //Pointer to unit occupying the location.
     bool isRR;      //Set true if location is on railroad line
+    Unit *occUnit;  //Pointer to unit occupying the location.
 };
 
 //Global Constants - Math, Science, Conversions, 2D Array Sizes
@@ -34,15 +34,32 @@ struct Location
 
 int main(int argc, char** argv) 
 {
-    fstream txt("txt_input.txt", ios::in);
-    fstream bin("Setup.txt", ios::out | ios::binary);
-    Location stream; 
+    fstream txt("text_input.txt", ios::in);
+    fstream bin("Setup.dat", ios::out | ios::binary);
+    Location stream;
+    string ptrRaw;
+    string tmpBool;
     
     do
     {
-        txt.read(reinterpret_cast<char *>(&stream), sizeof(stream));
+        //Get data from text file
+        getline(txt, stream.type);
+        getline(txt, tmpBool);
+        tmpBool=="0"?stream.isOcp = false:stream.isOcp=true;
+        getline(txt, tmpBool);
+        tmpBool=="0"?stream.isRR = false:stream.isRR=true;
+        getline(txt, ptrRaw);
+        if(ptrRaw == "0" || ptrRaw == "NULL")
+            stream.occUnit = NULL;
+        else
+            stream.occUnit = reinterpret_cast<Unit*>(strtol(ptrRaw.c_str(),NULL,0));
+        
+        //Write out data to binary file
         bin.write(reinterpret_cast<char *>(&stream), sizeof(stream));
     }while(!txt.eof());
+    
+    txt.close();
+    bin.close();
 
     return 0;
 }
