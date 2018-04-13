60bb9ecd7cfa788543c0c27294913134be6f7ea0
Kevin Maltz <31598741+kevmaltz@users.noreply.github.com>
2018-04-12 20:33:41 -0700

    .

--- Proj/Project_1/P1_The_War_Game/main.cpp
+++ Proj/Project_1/P1_The_War_Game/main.cpp
@@ -70,6 +70,7 @@ int main(int argc, char** argv)
     
     //Set player(s) and return number of human players
     nPlyr = stPlyrs(plyr1, plyr2, ai);
+    //TODO - Display the board, right now with no pieces.
     //TODO - Allow players to set up pieces
     
     
