702be85cecc2a4a0a5ed0be58bf82ceb848ecd1d
Kevin Maltz <kevmaltz@yahoo.com>
2018-05-29 22:06:30 -0700

    Create Board & Player class headers

---
+++ Proj/Project_2/P2_The_War_Game_OOP/Board.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Board.h
+ * Author: Kevin Maltz
+ *
+ * Created on May 29, 2018, 9:57 PM
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+
+
+#endif /* BOARD_H */
+
